149967da1a51b9fee98a15f56c25707937c0c7f6
Moosees <linus.almgren@gmail.com>
2019-09-04 23:44:15 +0200

    Add firebase auth listener to redirect logged in users main page

--- src/index.js
+++ src/index.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter
+} from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import firebase from './firebase/firebase';
 import * as serviceWorker from './serviceWorker';
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
-  </Router>
-);
+class Root extends Component {
+  componentDidMount() {
+    const { history } = this.props;
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        history.push('/');
+      }
+    });
+  }
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
+  <Router>
+    <RootWithAuth />
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
