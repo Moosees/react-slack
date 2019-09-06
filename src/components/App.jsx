43caf3ad43067cf647c346532c5728965c618d34
Moosees <linus.almgren@gmail.com>
2019-09-06 23:26:27 +0200

    Add main app skeleton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
 import './App.css';
+import ColorPanel from './ColorPanel/ColorPanel';
+import Messages from './Messages/Messages';
+import MetaPanel from './MetaPanel/MetaPanel';
+import SidePanel from './SidePanel/SidePanel';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Grid columns="equal" className="app">
+      <ColorPanel />
+      <SidePanel />
+      <Grid.Column style={{ marginLeft: '320' }}>
+        <Messages />
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <MetaPanel />
+      </Grid.Column>
+    </Grid>
+  );
 };
 
 export default App;
