9a14ea08e79787fab0e4152bb6aeb77445c7dd30
Moosees <linus.almgren@gmail.com>
2019-10-15 22:56:14 +0200

    Add meta panel layout

--- src/components/MetaPanel/MetaPanel.jsx
+++ src/components/MetaPanel/MetaPanel.jsx
@@ -1,9 +1,76 @@
 import React, { Component } from 'react';
+import { Segment, Header, Accordion, Icon } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 
 class MetaPanel extends Component {
+  state = {
+    activeIndex: 0
+  };
+
+  setActiveIndex = (evt, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+    this.setState({ activeIndex: newIndex });
+  };
+
   render() {
-    return <div>MetaPanel</div>;
+    const { activeIndex } = this.state;
+    const { currentChannel, isPrivateChannel } = this.props;
+
+    return isPrivateChannel ? null : (
+      <Segment key={currentChannel.id}>
+        <Header as="h3" attached="top">
+          About # {currentChannel.name}
+        </Header>
+        <Accordion styled attached="true">
+          <Accordion.Title
+            active={activeIndex === 0}
+            index={0}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name="dropdown" />
+            <Icon name="info" />
+            Channel Details
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+            channel details
+          </Accordion.Content>
+          <Accordion.Title
+            active={activeIndex === 1}
+            index={1}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name="dropdown" />
+            <Icon name="user circle" />
+            Top Posters
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 1}>
+            top posters
+          </Accordion.Content>
+          <Accordion.Title
+            active={activeIndex === 2}
+            index={2}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name="dropdown" />
+            <Icon name="pencil alternate" />
+            Created By
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 2}>
+            created by
+          </Accordion.Content>
+        </Accordion>
+      </Segment>
+    );
   }
 }
 
-export default MetaPanel;
+const mapStateToProps = ({
+  channel: { currentChannel, isPrivateChannel }
+}) => ({
+  currentChannel,
+  isPrivateChannel
+});
+
+export default connect(mapStateToProps)(MetaPanel);
